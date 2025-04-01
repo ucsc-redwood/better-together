b663575d294a0c919ad48a0f53a4ec32f1cb4069
Yanwen Xu <xuyanwen2012@gmail.com>
2025-03-31 19:07:04 -0700

    bm

--- play/ndarray/bm_main.cpp
+++ play/ndarray/bm_main.cpp
@@ -8,27 +8,62 @@ constexpr auto kDefaultInputFile = "cifar10_images/img_00005.npy";
 // static void BM_Stage_1(benchmark::State& state) {
 //   const auto num_threads = state.range(0);
 
-//   cifar_dense::AppData appdata(kDefaultInputFile);
+//   cifar_dense::AppDataBatch appdata(kDefaultInputFile);
 //   for (auto _ : state) {
-//     omp::dispatch_single_stage(appdata, 1, num_threads);
+//     omp::dispatch_multi_stage<ProcessorType::kLittleCore>(num_threads, appdata, 1, 1);
 //   }
 // }
 
-// BENCHMARK(BM_Stage_1)->DenseRange(1, 8)->Unit(benchmark::kMillisecond);
+// BENCHMARK(BM_Stage_1)->DenseRange(1, 6)->Unit(benchmark::kMillisecond);
 
-static void BM_Stage_1(benchmark::State& state) {
-  const auto num_threads = state.range(0);
-
-  cifar_dense::AppDataBatch appdata(kDefaultInputFile);
-  for (auto _ : state) {
-    omp::dispatch_multi_stage<ProcessorType::kLittleCore>(num_threads, appdata, 1, 1);
+const char* name_of_processor_type(ProcessorType pt) {
+  switch (pt) {
+    case ProcessorType::kLittleCore:
+      return "little";
+    case ProcessorType::kMediumCore:
+      return "medium";
+    case ProcessorType::kBigCore:
+      return "big";
   }
 }
 
-BENCHMARK(BM_Stage_1)->DenseRange(1, 6)->Unit(benchmark::kMillisecond);
+template <int Stage, ProcessorType PT>
+void register_stage_benchmark(const std::vector<int>& cores) {
+  for (size_t i = 1; i <= cores.size(); ++i) {
+    std::string benchmark_name =
+        "OMP_CifarDense/Stage" + std::to_string(Stage) + "_" + name_of_processor_type(PT);
+
+    benchmark::RegisterBenchmark(benchmark_name.c_str(),
+                                 [i, cores](benchmark::State& state) {
+                                   cifar_dense::AppDataBatch appdata(kDefaultInputFile);
+                                   for (auto _ : state) {
+                                     int num_threads = static_cast<int>(i);
+                                     omp::dispatch_multi_stage<PT>(
+                                         num_threads, appdata, Stage, Stage);
+                                   }
+                                 })
+        ->Arg(i)
+        ->Unit(benchmark::kMillisecond);
+  }
+}
 
 int main(int argc, char** argv) {
   parse_args(argc, argv);
+
+#define REGISTER_STAGE(STAGE)                                                  \
+  register_stage_benchmark<STAGE, ProcessorType::kLittleCore>(g_little_cores); \
+  register_stage_benchmark<STAGE, ProcessorType::kMediumCore>(g_medium_cores); \
+  register_stage_benchmark<STAGE, ProcessorType::kBigCore>(g_big_cores)
+
+  REGISTER_STAGE(1);
+  REGISTER_STAGE(2);
+  REGISTER_STAGE(3);
+  REGISTER_STAGE(4);
+  REGISTER_STAGE(5);
+  REGISTER_STAGE(6);
+  REGISTER_STAGE(7);
+  REGISTER_STAGE(8);
+  REGISTER_STAGE(9);
 
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
