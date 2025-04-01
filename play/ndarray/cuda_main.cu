3d6e1c3a05f85f4e616ec8247d4e98db294e1fa8
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-01 15:43:24 -0700

    s

--- play/ndarray/cuda_main.cu
+++ play/ndarray/cuda_main.cu
@@ -7,22 +7,27 @@
 #include "builtin-apps/common/cuda/manager.cuh"
 #include "cuda/dispatchers.cuh"
 
-// Global CUDA objects to ensure proper lifetime and thread visibility
-// cuda::CudaManager g_cuda_mgr;
-// std::unique_ptr<cuda::DeviceModelData> g_device_model_data;
+#define PREPARE_DATA                                        \
+  cuda::CudaManager mgr;                                    \
+  cifar_dense::AppDataBatch batched_appdata(&mgr.get_mr()); \
+  const cuda::DeviceModelData d_model_data(cifar_dense::AppDataBatch::get_model());
 
 int main(int argc, char** argv) {
   parse_args(argc, argv);
 
   spdlog::set_level(spdlog::level::from_str(g_spdlog_log_level));
 
-  cuda::CudaManager mgr;
-  const cuda::DeviceModelData d_model_data(cifar_dense::AppDataBatch::get_model());
+  // cuda::CudaManager mgr;
+  // const cuda::DeviceModelData d_model_data(cifar_dense::AppDataBatch::get_model());
 
-  cifar_dense::AppDataBatch batched_appdata(&mgr.get_mr());
+  // cifar_dense::AppDataBatch batched_appdata(&mgr.get_mr());
+
+  PREPARE_DATA;
 
   nvtxRangePushA("Compute");
   cuda::dispatch_multi_stage(batched_appdata, d_model_data, 1, 9, mgr);
+  CheckCuda(cudaDeviceSynchronize());
+
   nvtxRangePop();
 
   cifar_dense::print_batch_predictions(batched_appdata, 10);
