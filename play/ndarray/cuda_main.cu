f75a6e5adc5851fa8c3c6368d563786fac43c9c0
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-01 14:49:46 -0700

    a

--- play/ndarray/cuda_main.cu
+++ play/ndarray/cuda_main.cu
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
   cifar_dense::AppDataBatch batched_appdata(&mgr.get_mr());
 
   omp::dispatch_multi_stage<ProcessorType::kLittleCore>(4, batched_appdata, 1, 3);
-  cuda::dispatch_multi_stage(batched_appdata, d_model_data, 4, 6, mgr);
+  cuda::dispatch_multi_stage(batched_appdata, d_model_data, 4, 9, mgr);
 
   cifar_dense::print_batch_predictions(batched_appdata, 10);
 
