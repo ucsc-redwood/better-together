9226e3767b4b812fbfeec51e3294ff4086c69799
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-09 10:30:02 -0700

    cifar dense vk

--- play/ndarray/vulkan/dispatcher.hpp
+++ play/ndarray/vulkan/dispatcher.hpp
@@ -185,6 +185,506 @@ class VulkanDispatcher final {
     seq->wait_for_fence();
   }
 
+  void run_stage_2(cifar_dense::AppDataBatch& appdata) {
+    auto algo = cached_algorithms.at("maxpool2d").get();
+
+    algo->update_descriptor_set(0,
+                                {
+                                    engine.get_buffer_info(appdata.u_conv1_out.vec()),
+                                    engine.get_buffer_info(appdata.u_pool1_out.vec()),
+                                });
+
+    const auto& in_shape = appdata.u_conv1_out.shape();  // [128, 16, 32, 32]
+
+    constexpr int32_t padding = 2;
+    constexpr int32_t stride = 2;
+    constexpr int32_t pool_h = 2;
+    constexpr int32_t pool_w = 2;
+
+    const int32_t N = in_shape[0];  // batch, 128
+    const int32_t C = in_shape[1];  // in channels, 16
+    const int32_t H = in_shape[2];  // in height, 32
+    const int32_t W = in_shape[3];  // in width, 32
+
+    const int32_t P = (H + 2 * padding - pool_h) / stride + 1;
+    const int32_t Q = (W + 2 * padding - pool_w) / stride + 1;
+    const int32_t PQ = P * Q;
+
+    algo->update_push_constant(MaxpoolPushConstants{
+        .N = N,
+        .C = C,
+        .H = H,
+        .W = W,
+        .pool_h = pool_h,
+        .pool_w = pool_w,
+        .stride = stride,
+        .padding = padding,
+    });
+
+    LOG_KERNEL(LogKernelType::kVK, 2, &appdata);
+
+    seq->cmd_begin();
+    algo->record_bind_core(seq->get_handle(), 0);
+    algo->record_bind_push(seq->get_handle());
+    algo->record_dispatch(seq->get_handle(),
+                          {static_cast<uint32_t>(kiss_vk::div_ceil(PQ, 256)),
+                           static_cast<uint32_t>(C),
+                           static_cast<uint32_t>(N)});
+    seq->cmd_end();
+
+    seq->reset_fence();
+    seq->submit();
+    seq->wait_for_fence();
+  }
+
+  void run_stage_3(cifar_dense::AppDataBatch& appdata) {
+    auto algo = cached_algorithms.at("conv2d").get();
+
+    algo->update_descriptor_set(0,
+                                {
+                                    engine.get_buffer_info(appdata.u_pool1_out.vec()),
+                                    engine.get_buffer_info(appdata.u_conv2_w.vec()),
+                                    engine.get_buffer_info(appdata.u_conv2_b.vec()),
+                                    engine.get_buffer_info(appdata.u_conv2_out.vec()),
+                                });
+
+    const auto& in_shape = appdata.u_pool1_out.shape();   // [128, 16, 16, 16]
+    const auto& w_shape = appdata.u_conv2_w.shape();      // [32, 16, 3, 3]
+    const auto& out_shape = appdata.u_conv2_out.shape();  // [128, 32, 16, 16]
+
+    const int32_t N = in_shape[0];   // batch, 128
+    const int32_t C = in_shape[1];   // in channels, 16
+    const int32_t H = in_shape[2];   // in height, 16
+    const int32_t W = in_shape[3];   // in width, 16
+    const int32_t R = w_shape[2];    // kernel height, 3
+    const int32_t S = w_shape[3];    // kernel width, 3
+    const int32_t K = out_shape[1];  // out channels, 32
+
+    constexpr int32_t padding = 0;
+    constexpr int32_t stride = 1;
+    constexpr int32_t apply_relu = 1;
+    const int32_t P = (H + 2 * padding - R) / stride + 1;
+    const int32_t Q = (W + 2 * padding - S) / stride + 1;
+    const int32_t PQ = P * Q;
+
+    algo->update_push_constant(Conv2dPushConstants{
+        .N = N,
+        .C = C,
+        .H = H,
+        .W = W,
+        .K = K,
+        .R = R,
+        .S = S,
+        .stride = stride,
+        .padding = padding,
+        .apply_relu = apply_relu,
+    });
+
+    LOG_KERNEL(LogKernelType::kVK, 3, &appdata);
+
+    seq->cmd_begin();
+    algo->record_bind_core(seq->get_handle(), 0);
+    algo->record_bind_push(seq->get_handle());
+    algo->record_dispatch(seq->get_handle(),
+                          {static_cast<uint32_t>(kiss_vk::div_ceil(PQ, 256)),
+                           static_cast<uint32_t>(K),
+                           static_cast<uint32_t>(N)});
+    seq->cmd_end();
+
+    seq->reset_fence();
+    seq->submit();
+    seq->wait_for_fence();
+  }
+
+  void run_stage_4(cifar_dense::AppDataBatch& appdata) {
+    auto algo = cached_algorithms.at("maxpool2d").get();
+
+    algo->update_descriptor_set(0,
+                                {
+                                    engine.get_buffer_info(appdata.u_conv2_out.vec()),
+                                    engine.get_buffer_info(appdata.u_pool2_out.vec()),
+                                });
+
+    const auto& in_shape = appdata.u_conv2_out.shape();  // [128, 32, 16, 16]
+
+    constexpr int32_t padding = 2;
+    constexpr int32_t stride = 2;
+    constexpr int32_t pool_h = 2;
+    constexpr int32_t pool_w = 2;
+
+    const int32_t N = in_shape[0];  // batch, 128
+    const int32_t C = in_shape[1];  // in channels, 32
+    const int32_t H = in_shape[2];  // in height, 16
+    const int32_t W = in_shape[3];  // in width, 16
+
+    const int32_t P = (H + 2 * padding - pool_h) / stride + 1;
+    const int32_t Q = (W + 2 * padding - pool_w) / stride + 1;
+    const int32_t PQ = P * Q;
+
+    algo->update_push_constant(MaxpoolPushConstants{
+        .N = N,
+        .C = C,
+        .H = H,
+        .W = W,
+        .pool_h = pool_h,
+        .pool_w = pool_w,
+        .stride = stride,
+        .padding = padding,
+    });
+
+    LOG_KERNEL(LogKernelType::kVK, 4, &appdata);
+
+    seq->cmd_begin();
+    algo->record_bind_core(seq->get_handle(), 0);
+    algo->record_bind_push(seq->get_handle());
+    algo->record_dispatch(seq->get_handle(),
+                          {static_cast<uint32_t>(kiss_vk::div_ceil(PQ, 256)),
+                           static_cast<uint32_t>(C),
+                           static_cast<uint32_t>(N)});
+    seq->cmd_end();
+
+    seq->reset_fence();
+    seq->submit();
+    seq->wait_for_fence();
+  }
+
+  void run_stage_5(cifar_dense::AppDataBatch& appdata) {
+    auto algo = cached_algorithms.at("conv2d").get();
+
+    algo->update_descriptor_set(0,
+                                {
+                                    engine.get_buffer_info(appdata.u_pool2_out.vec()),
+                                    engine.get_buffer_info(appdata.u_conv3_w.vec()),
+                                    engine.get_buffer_info(appdata.u_conv3_b.vec()),
+                                    engine.get_buffer_info(appdata.u_conv3_out.vec()),
+                                });
+
+    const auto& in_shape = appdata.u_pool2_out.shape();   // [128, 32, 8, 8]
+    const auto& w_shape = appdata.u_conv3_w.shape();      // [64, 32, 3, 3]
+    const auto& out_shape = appdata.u_conv3_out.shape();  // [128, 64, 8, 8]
+
+    const int32_t N = in_shape[0];   // batch, 128
+    const int32_t C = in_shape[1];   // in channels, 32
+    const int32_t H = in_shape[2];   // in height, 8
+    const int32_t W = in_shape[3];   // in width, 8
+    const int32_t R = w_shape[2];    // kernel height, 3
+    const int32_t S = w_shape[3];    // kernel width, 3
+    const int32_t K = out_shape[1];  // out channels, 64
+
+    constexpr int32_t padding = 0;
+    constexpr int32_t stride = 1;
+    constexpr int32_t apply_relu = 1;
+    const int32_t P = (H + 2 * padding - R) / stride + 1;
+    const int32_t Q = (W + 2 * padding - S) / stride + 1;
+    const int32_t PQ = P * Q;
+
+    algo->update_push_constant(Conv2dPushConstants{
+        .N = N,
+        .C = C,
+        .H = H,
+        .W = W,
+        .K = K,
+        .R = R,
+        .S = S,
+        .stride = stride,
+        .padding = padding,
+        .apply_relu = apply_relu,
+    });
+
+    LOG_KERNEL(LogKernelType::kVK, 5, &appdata);
+
+    seq->cmd_begin();
+    algo->record_bind_core(seq->get_handle(), 0);
+    algo->record_bind_push(seq->get_handle());
+    algo->record_dispatch(seq->get_handle(),
+                          {static_cast<uint32_t>(kiss_vk::div_ceil(PQ, 256)),
+                           static_cast<uint32_t>(K),
+                           static_cast<uint32_t>(N)});
+    seq->cmd_end();
+
+    seq->reset_fence();
+    seq->submit();
+    seq->wait_for_fence();
+  }
+
+  void run_stage_6(cifar_dense::AppDataBatch& appdata) {
+    auto algo = cached_algorithms.at("conv2d").get();
+
+    algo->update_descriptor_set(0,
+                                {
+                                    engine.get_buffer_info(appdata.u_conv3_out.vec()),
+                                    engine.get_buffer_info(appdata.u_conv4_w.vec()),
+                                    engine.get_buffer_info(appdata.u_conv4_b.vec()),
+                                    engine.get_buffer_info(appdata.u_conv4_out.vec()),
+                                });
+
+    const auto& in_shape = appdata.u_conv3_out.shape();   // [128, 64, 8, 8]
+    const auto& w_shape = appdata.u_conv4_w.shape();      // [64, 64, 3, 3]
+    const auto& out_shape = appdata.u_conv4_out.shape();  // [128, 64, 8, 8]
+
+    const int32_t N = in_shape[0];   // batch, 128
+    const int32_t C = in_shape[1];   // in channels, 64
+    const int32_t H = in_shape[2];   // in height, 8
+    const int32_t W = in_shape[3];   // in width, 8
+    const int32_t R = w_shape[2];    // kernel height, 3
+    const int32_t S = w_shape[3];    // kernel width, 3
+    const int32_t K = out_shape[1];  // out channels, 64
+
+    constexpr int32_t padding = 0;
+    constexpr int32_t stride = 1;
+    constexpr int32_t apply_relu = 1;
+    const int32_t P = (H + 2 * padding - R) / stride + 1;
+    const int32_t Q = (W + 2 * padding - S) / stride + 1;
+    const int32_t PQ = P * Q;
+
+    algo->update_push_constant(Conv2dPushConstants{
+        .N = N,
+        .C = C,
+        .H = H,
+        .W = W,
+        .K = K,
+        .R = R,
+        .S = S,
+        .stride = stride,
+        .padding = padding,
+        .apply_relu = apply_relu,
+    });
+
+    LOG_KERNEL(LogKernelType::kVK, 6, &appdata);
+
+    seq->cmd_begin();
+    algo->record_bind_core(seq->get_handle(), 0);
+    algo->record_bind_push(seq->get_handle());
+    algo->record_dispatch(seq->get_handle(),
+                          {static_cast<uint32_t>(kiss_vk::div_ceil(PQ, 256)),
+                           static_cast<uint32_t>(K),
+                           static_cast<uint32_t>(N)});
+    seq->cmd_end();
+
+    seq->reset_fence();
+    seq->submit();
+    seq->wait_for_fence();
+  }
+
+  void run_stage_7(cifar_dense::AppDataBatch& appdata) {
+    auto algo = cached_algorithms.at("conv2d").get();
+
+    algo->update_descriptor_set(0,
+                                {
+                                    engine.get_buffer_info(appdata.u_conv4_out.vec()),
+                                    engine.get_buffer_info(appdata.u_conv5_w.vec()),
+                                    engine.get_buffer_info(appdata.u_conv5_b.vec()),
+                                    engine.get_buffer_info(appdata.u_conv5_out.vec()),
+                                });
+
+    const auto& in_shape = appdata.u_conv4_out.shape();   // [128, 64, 8, 8]
+    const auto& w_shape = appdata.u_conv5_w.shape();      // [64, 64, 3, 3]
+    const auto& out_shape = appdata.u_conv5_out.shape();  // [128, 64, 8, 8]
+
+    const int32_t N = in_shape[0];   // batch, 128
+    const int32_t C = in_shape[1];   // in channels, 64
+    const int32_t H = in_shape[2];   // in height, 8
+    const int32_t W = in_shape[3];   // in width, 8
+    const int32_t R = w_shape[2];    // kernel height, 3
+    const int32_t S = w_shape[3];    // kernel width, 3
+    const int32_t K = out_shape[1];  // out channels, 64
+
+    constexpr int32_t padding = 0;
+    constexpr int32_t stride = 1;
+    constexpr int32_t apply_relu = 1;
+    const int32_t P = (H + 2 * padding - R) / stride + 1;
+    const int32_t Q = (W + 2 * padding - S) / stride + 1;
+    const int32_t PQ = P * Q;
+
+    algo->update_push_constant(Conv2dPushConstants{
+        .N = N,
+        .C = C,
+        .H = H,
+        .W = W,
+        .K = K,
+        .R = R,
+        .S = S,
+        .stride = stride,
+        .padding = padding,
+        .apply_relu = apply_relu,
+    });
+
+    LOG_KERNEL(LogKernelType::kVK, 7, &appdata);
+
+    seq->cmd_begin();
+    algo->record_bind_core(seq->get_handle(), 0);
+    algo->record_bind_push(seq->get_handle());
+    algo->record_dispatch(seq->get_handle(),
+                          {static_cast<uint32_t>(kiss_vk::div_ceil(PQ, 256)),
+                           static_cast<uint32_t>(K),
+                           static_cast<uint32_t>(N)});
+    seq->cmd_end();
+
+    seq->reset_fence();
+    seq->submit();
+    seq->wait_for_fence();
+  }
+
+  void run_stage_8(cifar_dense::AppDataBatch& appdata) {
+    auto algo = cached_algorithms.at("maxpool2d").get();
+
+    algo->update_descriptor_set(0,
+                                {
+                                    engine.get_buffer_info(appdata.u_conv5_out.vec()),
+                                    engine.get_buffer_info(appdata.u_pool3_out.vec()),
+                                });
+
+    const auto& in_shape = appdata.u_conv5_out.shape();  // [128, 64, 8, 8]
+
+    constexpr int32_t padding = 2;
+    constexpr int32_t stride = 2;
+    constexpr int32_t pool_h = 2;
+    constexpr int32_t pool_w = 2;
+
+    const int32_t N = in_shape[0];  // batch, 128
+    const int32_t C = in_shape[1];  // in channels, 64
+    const int32_t H = in_shape[2];  // in height, 8
+    const int32_t W = in_shape[3];  // in width, 8
+
+    const int32_t P = (H + 2 * padding - pool_h) / stride + 1;
+    const int32_t Q = (W + 2 * padding - pool_w) / stride + 1;
+    const int32_t PQ = P * Q;
+
+    algo->update_push_constant(MaxpoolPushConstants{
+        .N = N,
+        .C = C,
+        .H = H,
+        .W = W,
+        .pool_h = pool_h,
+        .pool_w = pool_w,
+        .stride = stride,
+        .padding = padding,
+    });
+
+    LOG_KERNEL(LogKernelType::kVK, 8, &appdata);
+
+    seq->cmd_begin();
+    algo->record_bind_core(seq->get_handle(), 0);
+    algo->record_bind_push(seq->get_handle());
+    algo->record_dispatch(seq->get_handle(),
+                          {static_cast<uint32_t>(kiss_vk::div_ceil(PQ, 256)),
+                           static_cast<uint32_t>(C),
+                           static_cast<uint32_t>(N)});
+    seq->cmd_end();
+
+    seq->reset_fence();
+    seq->submit();
+    seq->wait_for_fence();
+  }
+
+  void run_stage_9(cifar_dense::AppDataBatch& appdata) {
+    auto algo = cached_algorithms.at("linear").get();
+
+    algo->update_descriptor_set(0,
+                                {
+                                    engine.get_buffer_info(appdata.u_pool3_out.vec()),
+                                    engine.get_buffer_info(appdata.u_linear_w.vec()),
+                                    engine.get_buffer_info(appdata.u_linear_b.vec()),
+                                    engine.get_buffer_info(appdata.u_linear_out.vec()),
+                                });
+
+    const auto& in_shape = appdata.u_pool3_out.shape();  // [128, 64, 4, 4]
+    const auto& w_shape = appdata.u_linear_w.shape();    // [10, 1024]
+
+    // For the linear layer, we need to flatten the 4D tensor to 2D
+    const int32_t N = in_shape[0];  // batch size, 128
+    const int32_t C = in_shape[1];  // channels, 64
+    const int32_t H = in_shape[2];  // height, 4
+    const int32_t W = in_shape[3];  // width, 4
+
+    // 64*4*4 = 1024
+    const int32_t in_features = C * H * W;
+    const int32_t out_features = w_shape[0];  // output features, 10
+
+    algo->update_push_constant(LinearPushConstants{
+        .N = N,
+        .in_features = in_features,
+        .out_features = out_features,
+    });
+
+    LOG_KERNEL(LogKernelType::kVK, 9, &appdata);
+
+    const uint32_t total_outputs = N * out_features;
+    const uint32_t num_blocks = (total_outputs + 255) / 256;
+
+    seq->cmd_begin();
+    algo->record_bind_core(seq->get_handle(), 0);
+    algo->record_bind_push(seq->get_handle());
+    algo->record_dispatch(seq->get_handle(), {num_blocks, 1, 1});
+    seq->cmd_end();
+
+    seq->reset_fence();
+    seq->submit();
+    seq->wait_for_fence();
+  }
+
+  // void dispatch_multi_stage(cifar_dense::AppDataBatch& data,
+  //                           const int start_stage,
+  //                           const int end_stage) {
+  //   if (start_stage < 1 || end_stage > 9) throw std::out_of_range("Invalid stage");
+
+  //   for (int stage = start_stage; stage <= end_stage; stage++) {
+  //     switch (stage) {
+  //       case 1:
+  //         run_stage_1(data);
+  //         break;
+  //       case 2:
+  //         run_stage_2(data);
+  //         break;
+  //       case 3:
+  //         run_stage_3(data);
+  //         break;
+  //       case 4:
+  //         run_stage_4(data);
+  //         break;
+  //       case 5:
+  //         run_stage_5(data);
+  //         break;
+  //       case 6:
+  //         run_stage_6(data);
+  //         break;
+  //       case 7:
+  //         run_stage_7(data);
+  //         break;
+  //       case 8:
+  //         run_stage_8(data);
+  //         break;
+  //       case 9:
+  //         run_stage_9(data);
+  //         break;
+  //     }
+  //   }
+  // }
+
+  using StageFn = void (VulkanDispatcher::*)(cifar_dense::AppDataBatch&);
+
+  static constexpr std::array<StageFn, 9> stage_functions = {
+      &VulkanDispatcher::run_stage_1,
+      &VulkanDispatcher::run_stage_2,
+      &VulkanDispatcher::run_stage_3,
+      &VulkanDispatcher::run_stage_4,
+      &VulkanDispatcher::run_stage_5,
+      &VulkanDispatcher::run_stage_6,
+      &VulkanDispatcher::run_stage_7,
+      &VulkanDispatcher::run_stage_8,
+      &VulkanDispatcher::run_stage_9,
+  };
+
+  void dispatch_multi_stage(cifar_dense::AppDataBatch& data,
+                            const int start_stage,
+                            const int end_stage) {
+    if (start_stage < 1 || end_stage > 9) throw std::out_of_range("Invalid stage");
+
+    for (int stage = start_stage; stage <= end_stage; stage++) {
+      (this->*stage_functions[stage - 1])(data);
+    }
+  }
+
  private:
   kiss_vk::Engine engine;
   std::shared_ptr<kiss_vk::Sequence> seq;
