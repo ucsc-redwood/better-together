fc256722ef66b2b0833a268158a2634c86fa8479
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-10 14:25:48 -0700

    save

--- builtin-apps/cifar-sparse/vulkan/bm_main.cpp
+++ builtin-apps/cifar-sparse/vulkan/bm_main.cpp
@@ -36,58 +36,32 @@ static void BM_run_OMP_baseline(benchmark::State& state) {
 
 BENCHMARK(BM_run_OMP_baseline)->Unit(benchmark::kMillisecond);
 
-// static void BM_run_OMP_stage(benchmark::State& state) {
-//   const int stage = state.range(0);
-//   const int num_threads = state.range(1);
-//   const ProcessorType core_type = static_cast<ProcessorType>(state.range(2));
+static void BM_run_OMP_stage(benchmark::State& state) {
+  const int stage = state.range(0);
 
-//   auto mr = std::pmr::new_delete_resource();
-//   cifar_sparse::v2::AppData appdata(mr);
+  cifar_sparse::vulkan::v2::VulkanDispatcher disp;
+  cifar_sparse::v2::AppData appdata(disp.get_mr());
 
-//   std::vector<int> cores_to_use;
-//   if (core_type == ProcessorType::kLittleCore) {
-//     cores_to_use = g_little_cores;
-//   } else if (core_type == ProcessorType::kMediumCore) {
-//     cores_to_use = g_medium_cores;
-//   } else {
-//     cores_to_use = g_big_cores;
-//   }
+  // Ensure stage is valid
+  if (stage < 1 || stage > 9) {
+    state.SkipWithError("Invalid stage number");
+    return;
+  }
 
-//   if (num_threads > (static_cast<int>(cores_to_use.size()))) {
-//     state.SkipWithMessage("");
-//     return;
-//   }
+  // Warm up to prevent first-run anomalies
+  disp.dispatch_multi_stage(appdata, 1, stage);
 
-//   // Ensure stage is valid
-//   if (stage < 1 || stage > 9) {
-//     state.SkipWithError("Invalid stage number");
-//     return;
-//   }
+  for (auto _ : state) {
+    disp.dispatch_multi_stage(appdata, stage, stage);
+  }
+}
 
-//   // Warm up to prevent first-run anomalies
-//   cifar_sparse::omp::v2::dispatch_multi_stage(cores_to_use, num_threads, appdata, 1, stage);
-
-//   for (auto _ : state) {
-//     cifar_sparse::omp::v2::dispatch_multi_stage(cores_to_use, num_threads, appdata, stage,
-//     stage);
-//   }
-// }
-
-// static void CustomArgs(benchmark::internal::Benchmark* b) {
-//   for (int stage = 1; stage <= 9; ++stage) {
-//     for (int num_threads = 1; num_threads <= 8; ++num_threads) {
-//       for (const ProcessorType core_type :
-//            {ProcessorType::kLittleCore, ProcessorType::kMediumCore, ProcessorType::kBigCore}) {
-//         b->Args({stage, num_threads, static_cast<int>(core_type)});
-//       }
-//     }
-//   }
-// }
-
-// BENCHMARK(BM_run_OMP_stage)->Apply(CustomArgs)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_run_OMP_stage)->DenseRange(1, 9)->Unit(benchmark::kMillisecond);
 
 int main(int argc, char** argv) {
   parse_args(argc, argv);
+
+  spdlog::set_level(spdlog::level::from_str(g_spdlog_log_level));
 
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
