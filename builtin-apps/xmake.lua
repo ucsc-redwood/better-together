29e5dcaded84bbb74a78bf9c7fbfbe55f75f16ff
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-10 14:15:32 -0700

    worked

--- builtin-apps/xmake.lua
+++ builtin-apps/xmake.lua
@@ -137,6 +137,19 @@ if has_config("use_vulkan") then
 	end
 
 
+	target("bm-mini-cifar-sparse-vk")
+	do
+		add_rules("common_flags", "vulkan_config", "run_on_android")
+		set_kind("binary")
+		add_files({
+			"cifar-sparse/vulkan/bm_main.cpp",
+		})
+
+		add_deps("builtin-apps-vulkan")
+		add_deps("builtin-apps")
+		
+		add_packages("benchmark")
+	end
 end
 
 -- ----------------------------------------------------------------------------
