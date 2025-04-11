d425931a961018c02af43b5d4e0a6bc2d176fc12
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-11 01:39:07 -0700

    s

--- scripts-v2/parse-pipe-bm/try.py
+++ scripts-v2/parse-pipe-bm/try.py
@@ -6,4 +6,9 @@ df = pd.read_pickle("parsed_data/3A021JEHN02756_benchmark.pkl")
 
 # Get a specific timing
 vk_stage1_time = get_benchmark_time(df, vendor="VK", stage=1)
+
+
+
+
+
 print(f"VK Stage 1 time: {vk_stage1_time} ms")
