7628fa889df113f27c0f0a0edaf0d11944739506
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-14 01:46:30 -0700

    x

--- builtin-apps/cifar-dense/vulkan/test_main.cpp
+++ builtin-apps/cifar-dense/vulkan/test_main.cpp
@@ -204,10 +204,6 @@ TEST(Stage9Test, Basic) {
 }
 
 int main(int argc, char **argv) {
-  parse_args(argc, argv);
-
-  spdlog::set_level(spdlog::level::from_str(g_spdlog_log_level));
-
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
