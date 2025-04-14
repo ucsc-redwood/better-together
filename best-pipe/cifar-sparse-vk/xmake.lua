60bc9b48d6bfa53c676cfcd47106f1d4d31e371d
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-13 23:27:36 -0700

    save

--- best-pipe/cifar-sparse-vk/xmake.lua
+++ best-pipe/cifar-sparse-vk/xmake.lua
@@ -1,5 +1,23 @@
 -- ----------------------------------------------------------------------------
 -- Run stages with interference (100 tasks)
+-- ----------------------------------------------------------------------------
+
+target("bm-baseline-cifar-sparse-vk")
+do
+	add_rules("common_flags", "vulkan_config", "run_on_android")
+	set_kind("binary")
+	add_files({
+		"bm_baseline.cpp",
+	})
+
+	add_deps("builtin-apps-vulkan")
+	add_deps("builtin-apps")
+
+	add_packages("benchmark")
+end
+
+-- ----------------------------------------------------------------------------
+-- Run stages with interference (100 tasks)
 -- Use this to generate Table, which will feed into optimizer
 -- ----------------------------------------------------------------------------
 
