42ce2356a09bc7f002724dc3f35de0d77b3785f2
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-14 10:56:27 -0700

    save

--- builtin-apps/tree/omp/xmake.lua
+++ builtin-apps/tree/omp/xmake.lua
@@ -17,3 +17,16 @@ do
 	add_packages("gtest")
 end
 
+target("bm-mini-tree-omp")
+do
+	add_rules("common_flags", "run_on_android")
+	set_group("test")
+	set_kind("binary")
+	add_files({
+		"./bm_mini.cpp",
+	})
+
+	add_deps("builtin-apps")
+		
+	add_packages("benchmark")
+end
