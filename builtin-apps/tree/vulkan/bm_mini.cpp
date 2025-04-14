48bd78f32004ba2fe7a5599c4fe9703c988e661e
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-14 10:47:43 -0700

    s

--- builtin-apps/tree/vulkan/bm_mini.cpp
+++ builtin-apps/tree/vulkan/bm_mini.cpp
@@ -3,6 +3,10 @@
 #include "../../app.hpp"
 #include "dispatchers.hpp"
 
+// ----------------------------------------------------------------
+// Initialize AppData
+// ----------------------------------------------------------------
+
 static void BM_InitializeTree(benchmark::State& state) {
   tree::vulkan::VulkanDispatcher disp;
 
@@ -15,6 +19,132 @@ static void BM_InitializeTree(benchmark::State& state) {
 }
 
 BENCHMARK(BM_InitializeTree)->Unit(benchmark::kMillisecond);
+
+// ----------------------------------------------------------------
+// Stage 1
+// ----------------------------------------------------------------
+
+static void BM_Stage1(benchmark::State& state) {
+  tree::vulkan::VulkanDispatcher disp;
+  tree::vulkan::VkAppData_Safe appdata(disp.get_mr());
+
+  // warm up
+  disp.run_stage_1(appdata);
+
+  for (auto _ : state) {
+    disp.run_stage_1(appdata);
+  }
+}
+
+BENCHMARK(BM_Stage1)->Unit(benchmark::kMillisecond);
+
+// ----------------------------------------------------------------
+// Stage 2
+// ----------------------------------------------------------------
+
+static void BM_Stage2(benchmark::State& state) {
+  tree::vulkan::VulkanDispatcher disp;
+  tree::vulkan::VkAppData_Safe appdata(disp.get_mr());
+
+  // warm up
+  disp.run_stage_2(appdata);
+
+  for (auto _ : state) {
+    disp.run_stage_2(appdata);
+  }
+}
+
+BENCHMARK(BM_Stage2)->Unit(benchmark::kMillisecond);
+
+// ----------------------------------------------------------------
+// Stage 3
+// ----------------------------------------------------------------
+
+static void BM_Stage3(benchmark::State& state) {
+  tree::vulkan::VulkanDispatcher disp;
+  tree::vulkan::VkAppData_Safe appdata(disp.get_mr());
+
+  // warm up
+  disp.run_stage_3(appdata);
+
+  for (auto _ : state) {
+    disp.run_stage_3(appdata);
+  }
+}
+
+BENCHMARK(BM_Stage3)->Unit(benchmark::kMillisecond);
+
+// ----------------------------------------------------------------
+// Stage 4
+// ----------------------------------------------------------------
+
+static void BM_Stage4(benchmark::State& state) {
+  tree::vulkan::VulkanDispatcher disp;
+  tree::vulkan::VkAppData_Safe appdata(disp.get_mr());
+
+  // warm up
+  disp.run_stage_4(appdata);
+
+  for (auto _ : state) {
+    disp.run_stage_4(appdata);
+  }
+}
+
+BENCHMARK(BM_Stage4)->Unit(benchmark::kMillisecond);
+
+// ----------------------------------------------------------------
+// Stage 5
+// ----------------------------------------------------------------
+
+static void BM_Stage5(benchmark::State& state) {
+  tree::vulkan::VulkanDispatcher disp;
+  tree::vulkan::VkAppData_Safe appdata(disp.get_mr());
+
+  // warm up
+  disp.run_stage_5(appdata);
+
+  for (auto _ : state) {
+    disp.run_stage_5(appdata);
+  }
+}
+
+BENCHMARK(BM_Stage5)->Unit(benchmark::kMillisecond);
+
+// ----------------------------------------------------------------
+// Stage 6
+// ----------------------------------------------------------------
+
+static void BM_Stage6(benchmark::State& state) {
+  tree::vulkan::VulkanDispatcher disp;
+  tree::vulkan::VkAppData_Safe appdata(disp.get_mr());
+
+  // warm up
+  disp.run_stage_6(appdata);
+
+  for (auto _ : state) {
+    disp.run_stage_6(appdata);
+  }
+}
+
+BENCHMARK(BM_Stage6)->Unit(benchmark::kMillisecond);
+
+// ----------------------------------------------------------------
+// Stage 7
+// ----------------------------------------------------------------
+
+static void BM_Stage7(benchmark::State& state) {
+  tree::vulkan::VulkanDispatcher disp;
+  tree::vulkan::VkAppData_Safe appdata(disp.get_mr());
+
+  // warm up
+  disp.run_stage_7(appdata);
+
+  for (auto _ : state) {
+    disp.run_stage_7(appdata);
+  }
+}
+
+BENCHMARK(BM_Stage7)->Unit(benchmark::kMillisecond);
 
 int main(int argc, char** argv) {
   parse_args(argc, argv);
