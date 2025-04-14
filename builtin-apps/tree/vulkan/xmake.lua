45eb0703e36de89f87eda21984edb55f93604bab
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-14 10:05:24 -0700

    s

--- builtin-apps/tree/vulkan/xmake.lua
+++ builtin-apps/tree/vulkan/xmake.lua
@@ -18,3 +18,19 @@ do
 	add_packages("gtest")
 end
 
+
+target("bm-mini-tree-vk")
+do
+	add_rules("common_flags", "vulkan_config", "run_on_android")
+	set_group("test")
+	set_kind("binary")
+	add_files({
+		"./bm_mini.cpp",
+	})
+
+	add_deps("builtin-apps-vulkan")
+	add_deps("builtin-apps")
+		
+	add_packages("benchmark")
+end
+
