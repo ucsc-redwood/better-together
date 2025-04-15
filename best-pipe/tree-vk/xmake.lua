41b072b983aa1a5de5d5f35df42740e42a6156d9
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-15 12:38:29 -0700

    s

--- best-pipe/tree-vk/xmake.lua
+++ best-pipe/tree-vk/xmake.lua
@@ -16,6 +16,19 @@ do
 	add_packages("benchmark")
 end
 
+target("bm-fully-tree-vk")
+do
+	add_rules("common_flags", "vulkan_config", "run_on_android")
+	set_kind("binary")
+	add_files({
+		"bm_fully_vs_normal.cpp",
+	})
+
+	add_deps("builtin-apps-vulkan")
+	add_deps("builtin-apps")
+end
+
+
 -- -- ----------------------------------------------------------------------------
 -- -- Run stages with interference (100 tasks)
 -- -- Use this to generate Table, which will feed into optimizer
