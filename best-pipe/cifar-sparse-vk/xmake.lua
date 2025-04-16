eb5b4f13ce28176431d4f89e386856075337cc0c
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-15 17:06:31 -0700

    s

--- best-pipe/cifar-sparse-vk/xmake.lua
+++ best-pipe/cifar-sparse-vk/xmake.lua
@@ -21,17 +21,17 @@ end
 -- Use this to generate Table, which will feed into optimizer
 -- ----------------------------------------------------------------------------
 
-target("bm-table-cifar-sparse-vk")
-do
-	add_rules("common_flags", "vulkan_config", "run_on_android")
-	set_kind("binary")
-	add_files({
-		"bm_for_table.cpp",
-	})
+-- target("bm-table-cifar-sparse-vk")
+-- do
+-- 	add_rules("common_flags", "vulkan_config", "run_on_android")
+-- 	set_kind("binary")
+-- 	add_files({
+-- 		"bm_for_table.cpp",
+-- 	})
 
-	add_deps("builtin-apps-vulkan")
-	add_deps("builtin-apps")
-end
+-- 	add_deps("builtin-apps-vulkan")
+-- 	add_deps("builtin-apps")
+-- end
 
 
 target("bm-fully-cifar-sparse-vk")
@@ -52,19 +52,19 @@ end
 -- The Result. 
 -- ----------------------------------------------------------------------------
 
-target("bm-schedule-cifar-sparse-vk")
-do
-	add_rules("common_flags", "vulkan_config", "run_on_android")
-	set_kind("binary")
-	add_files({
-		"bm_real_schedule.cpp",
-	})
+-- target("bm-schedule-cifar-sparse-vk")
+-- do
+-- 	add_rules("common_flags", "vulkan_config", "run_on_android")
+-- 	set_kind("binary")
+-- 	add_files({
+-- 		"bm_real_schedule.cpp",
+-- 	})
 
-	add_deps("builtin-apps-vulkan")
-	add_deps("builtin-apps")
+-- 	add_deps("builtin-apps-vulkan")
+-- 	add_deps("builtin-apps")
 		
-	add_packages("benchmark")
-end
+-- 	add_packages("benchmark")
+-- end
 
 -- ----------------------------------------------------------------------------
 -- Generate the Log/Graph of "schedules" (100 tasks)
