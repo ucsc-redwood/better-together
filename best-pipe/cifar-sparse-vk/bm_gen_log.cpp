9d9aa19debf8a3ed51a0202c5eed9a60c1b3b9b1
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-15 23:46:05 -0700

    s

--- best-pipe/cifar-sparse-vk/bm_gen_log.cpp
+++ best-pipe/cifar-sparse-vk/bm_gen_log.cpp
@@ -2,56 +2,7 @@
 
 #include "builtin-apps/app.hpp"
 #include "common.hpp"
-
-// ----------------------------------------------------------------------------
-// Specialized Schedules
-// ----------------------------------------------------------------------------
-
-struct Chunk {
-  ProcessorType core;
-  std::vector<int> indices;  // e.g., {0} or {1, 2, 3, 4, 5}
-};
-
-struct Schedule {
-  std::vector<Chunk> chunks;
-
-  [[nodiscard]] size_t n_chunks() const { return chunks.size(); }
-
-  void setup_record_manager() const {
-    std::vector<std::pair<int, ProcessorType>> chunk_records;
-
-    for (size_t i = 0; i < chunks.size(); ++i) {
-      chunk_records.push_back(std::make_pair(i, chunks[i].core));
-    }
-
-    RecordManager::instance().setup(kNumToProcess, chunk_records);
-  }
-
-  void print() const {
-    constexpr const char* kProcessorTypeNames[] = {"L", "M", "B", "V"};
-
-    for (const auto& chunk : chunks) {
-      std::cout << "[" << kProcessorTypeNames[static_cast<int>(chunk.core)] << "] ";
-      for (const auto& index : chunk.indices) {
-        std::cout << index << " ";
-      }
-      std::cout << std::endl;
-    }
-  }
-};
-
-std::vector<int>& get_cores_by_type(const ProcessorType core_type) {
-  switch (core_type) {
-    case ProcessorType::kLittleCore:
-      return g_little_cores;
-    case ProcessorType::kMediumCore:
-      return g_medium_cores;
-    case ProcessorType::kBigCore:
-      return g_big_cores;
-    default:
-      throw std::invalid_argument("Invalid core type");
-  }
-}
+#include "schedules.hpp"
 
 // ----------------------------------------------------------------------------
 // Schedule Auto
@@ -111,33 +62,11 @@ static void BM_pipe_cifar_sparse_vk_schedule_auto(const Schedule schedule) {
 // Main
 // ----------------------------------------------------------------------------
 
-#define BIG(...)                                                \
-  {                                                             \
-    .core = ProcessorType::kBigCore, .indices = { __VA_ARGS__ } \
-  }
-#define MED(...)                                                   \
-  {                                                                \
-    .core = ProcessorType::kMediumCore, .indices = { __VA_ARGS__ } \
-  }
-#define LIT(...)                                                   \
-  {                                                                \
-    .core = ProcessorType::kLittleCore, .indices = { __VA_ARGS__ } \
-  }
-#define GPU(...)                                               \
-  {                                                            \
-    .core = ProcessorType::kVulkan, .indices = { __VA_ARGS__ } \
-  }
-
-#define SCHEDULE(...)         \
-  {                           \
-    .chunks = { __VA_ARGS__ } \
-  }
-
 int main(int argc, char** argv) {
   PARSE_ARGS_BEGIN
 
-  int schedule_id = 0;
-  app.add_option("--schedule", schedule_id, "Schedule to run")->required();
+  // int schedule_id = 0;
+  // app.add_option("--schedule", schedule_id, "Schedule to run")->required();
 
   PARSE_ARGS_END
 
@@ -147,73 +76,15 @@ int main(int argc, char** argv) {
     return 0;
   }
 
-  assert(schedule_id >= 1 && schedule_id <= 10);
+  // assert(schedule_id >= 1 && schedule_id <= 10);
 
-  // clang-format off
-  std::array<Schedule, 10> schedules = {{
-      SCHEDULE(
-          BIG(0),
-          GPU(1, 2, 3, 4, 5),
-          MED(6, 7, 8)
-      ),
-      SCHEDULE(
-          BIG(0),
-          GPU(1, 2, 3, 4, 5),
-          MED(6, 7),
-          LIT(8)
-      ),
-      SCHEDULE(
-          BIG(0),
-          GPU(1, 2, 3, 4),
-          LIT(5),
-          MED(6, 7, 8)
-      ),
-      SCHEDULE(
-          MED(0),
-          GPU(1, 2, 3, 4, 5),
-          BIG(6, 7),
-          LIT(8)
-      ),
-      SCHEDULE(
-          MED(0),
-          GPU(1, 2, 3, 4),
-          LIT(5),
-          BIG(6, 7, 8)
-      ),
-      SCHEDULE(
-          MED(0),
-          GPU(1, 2, 3, 4, 5),
-          BIG(6, 7, 8)
-      ),
-      SCHEDULE(
-          GPU(0, 1, 2, 3),
-          BIG(4),
-          LIT(5),
-          MED(6, 7, 8)
-      ),
-      SCHEDULE(
-          GPU(0, 1, 2, 3),
-          MED(4),
-          LIT(5),
-          BIG(6, 7, 8)
-      ),
-      SCHEDULE(
-          GPU(0, 1, 2, 3),
-          LIT(4),
-          MED(5, 6),
-          BIG(7, 8)
-      ),
-      SCHEDULE(
-          GPU(0, 1, 2, 3),
-          MED(4, 5, 6),
-          BIG(7, 8)
-      ),
-  }};
-  // clang-format on
+  // Schedule schedule = schedules[schedule_id - 1];
 
-  Schedule schedule = schedules[schedule_id - 1];
+  for (const auto& schedule : device_3A021JEHN02756::schedules) {
+    BM_pipe_cifar_sparse_vk_schedule_auto(schedule);
 
-  BM_pipe_cifar_sparse_vk_schedule_auto(schedule);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
 
   return 0;
 }
