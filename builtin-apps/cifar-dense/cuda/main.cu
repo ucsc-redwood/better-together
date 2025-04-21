2645f2c0d58e38a5f880b159bc9a69276842d708
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-21 01:42:13 -0700

    s

--- builtin-apps/cifar-dense/cuda/main.cu
+++ builtin-apps/cifar-dense/cuda/main.cu
@@ -2,8 +2,17 @@
 #include "../../hex_dump.hpp"
 #include "dispatchers.cuh"
 
+#include <omp.h>
+
 int main(int argc, char** argv) {
   parse_args(argc, argv);
+
+  // print number of omp threads
+  printf("Number of OMP threads: %d\n", omp_get_max_threads());
+  #pragma omp parallel
+  {
+    printf("Thread ID: %d\n", omp_get_thread_num());
+  }
 
   spdlog::set_level(spdlog::level::from_str(g_spdlog_log_level));
 
