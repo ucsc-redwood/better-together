129cd2b646f19a5ab6033c01dd467bf0aec2ae1b
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-21 17:22:51 -0700

    s

--- builtin-apps/base_appdata.hpp
+++ builtin-apps/base_appdata.hpp
@@ -5,10 +5,15 @@
 struct BaseAppData {
   static uint32_t next_uid;
   uint32_t uid;
+  uint32_t initial_uid;
 
-  BaseAppData() : uid(next_uid++) {}
+  BaseAppData() : uid(next_uid++), initial_uid(uid) {}
 
   void reset() { uid = next_uid++; }
 
   [[nodiscard]] uint32_t get_uid() const { return uid; }
+
+  [[nodiscard]] uint32_t get_initial_uid() const { return initial_uid; }
 };
+
+inline uint32_t BaseAppData::next_uid = 0;
