845035f9ae1d7fc2762ea1ae9ff9af25036170ce
Yanwen Xu <xuyanwen2012@gmail.com>
2025-04-22 13:46:04 -0700

    s

---
+++ builtin-apps/cifar-dense/omp/main.cpp
@@ -0,0 +1,25 @@
+#include <omp.h>
+#include <spdlog/spdlog.h>
+
+#include "../../app.hpp"
+#include "../../hex_dump.hpp"
+#include "dispatchers.hpp"
+
+int main(int argc, char** argv) {
+  parse_args(argc, argv);
+
+  spdlog::set_level(spdlog::level::from_str(g_spdlog_log_level));
+
+  auto mr = std::pmr::new_delete_resource();
+  cifar_dense::AppData appdata(mr);
+
+  dumpCompressed(appdata.u_input.pmr_vec());
+
+  dumpCompressed(appdata.u_conv1_out.pmr_vec());
+
+  cifar_dense::omp::dispatch_stage(appdata, 1);
+
+  dumpCompressed(appdata.u_conv1_out.pmr_vec());
+
+  return 0;
+}
--- builtin-apps/cifar-dense/omp/xmake.lua
+++ builtin-apps/cifar-dense/omp/xmake.lua
@@ -2,6 +2,19 @@
 -- Test, just test if the dispatchers are working
 -- ----------------------------------------------------------------------------
 
+target("run-cifar-dense-omp")
+do
+	add_rules("common_flags", "run_on_android")
+	set_group("test")
+	set_kind("binary")
+	add_files({
+		"./main.cpp",
+	})
+
+	add_deps("builtin-apps")
+end
+
+
 target("test-cifar-dense-omp")
 do
 	add_rules("common_flags", "run_on_android")
